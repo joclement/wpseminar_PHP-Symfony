c9d15c653df7aacea4223940f10cba0e627c4110
Joris Clement <7713214+joclement@users.noreply.github.com>
2016-02-15 18:18:45 +0100

    Adds files generated during live presentation
    no modifications at other times

---
+++ php_live/hello_world_html.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title>Hello World PHP</title>
+</head>
+<?php
+
+echo "Hello World!";
+
+$numbe += 0;
+
+echo $a;
+
+if (0 === Null && -1 > Null) {
+  echo "geht doch";
+}
+
+function defaultmethod($param="al",$param2)
+{
+  echo "2 Variablen: $param ,$param2";
+}
+
+defaultmethod("a","b");
+defaultmethod("a");
+
+?>
+
+<body>
+</body>
+</html>
